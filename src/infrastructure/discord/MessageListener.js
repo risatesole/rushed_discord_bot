39ce81cac0f05a6cc76446169d4cd791b46dc588
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-02-23 23:13:47 -0400

    feat: print detailed user info including roles, server, and channel

--- src/infrastructure/discord/MessageListener.js
+++ src/infrastructure/discord/MessageListener.js
@@ -9,7 +9,26 @@ export default class MessageListener {
     this.client.on("messageCreate", async (message) => {
       if (message.author.bot) return;
 
-      if (message.content === "bot!hey") {
+      const username = message.author.username;
+      const servername = message.guild.name;
+      const serverid = message.guild.id;
+      const messagechannelname = message.channel.name;
+      const messagechannelid = message.channel.id;
+
+      console.log("username: ", username);
+      console.log("server name: ", servername);
+      console.log("serverid: ", serverid);
+      console.log("server channel name: ", messagechannelname);
+      console.log("server channel id: ", messagechannelid);
+
+      const member = message.guild.members.cache.get(message.author.id);
+      if (member) {
+        const roles = member.roles.cache.map((role) => role.name);
+        console.log("Roles:", roles);
+      }
+
+      console.log("#########################################");
+      if (message.content === "bot!") {
         const response = this.heyHandler.handle();
 
         await message.reply(response);
